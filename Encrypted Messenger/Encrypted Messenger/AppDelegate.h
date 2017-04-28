11fbd850fb2c0ec67fa62b9701ffc582c9d91a8e
Neil Nie <yongyang.nie@gmail.com>
2017-04-28 08:45:59 -0400

    Initial Commit

---
+++ Encrypted Messenger/Encrypted Messenger/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Encrypted Messenger
+//
+//  Created by Yongyang Nie on 4/28/17.
+//  Copyright © 2017 Yongyang Nie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Encrypted Messenger/Encrypted Messenger/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Encrypted Messenger
+//
+//  Created by Yongyang Nie on 4/28/17.
+//  Copyright © 2017 Yongyang Nie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
